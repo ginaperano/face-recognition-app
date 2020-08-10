ebf9a84ec2399a75b2e7f1a5062a995dcf9b69bd
Gina <hello@ginaperano.com>
2020-08-10 15:52:10 -0700

    Image Recognition API Updates

--- src/App.js
+++ src/App.js
@@ -34,23 +34,22 @@ class App extends Component {
   }
 
   onInputChange = (event) => {
-    this.setState(event.target.value);
+    this.setState({ input: event.target.value });
   }
 
   onButtonSubmit = () => {
     this.setState({ imageUrl: this.state.input })
     console.log('click');
     app.models.predict(
-      Clarifai.COLOR_MODEL,
-      "https://samples.clarifai.com/face-det.jpg"
-    ).then(
-      function (response) {
-        console.log(response);
-      },
-      function (err) {
-        // there was an error 
-      }
-    );
+      Clarifai.FACE_DETECT_MODEL,
+      this.state.input).then(
+        function (response) {
+          console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
+        },
+        function (err) {
+          // there was an error 
+        }
+      );
   }
   render() {
     return (
@@ -63,7 +62,7 @@ class App extends Component {
         <Rank />
         <ImageLinkForm onInputChange={this.onInputChange}
           onButtonSubmit={this.onButtonSubmit} />
-        <FaceRecognition imageUrl={this.state.ImageUrl} />
+        <FaceRecognition imageUrl={this.state.imageUrl} />
       </div>
     );
   }
