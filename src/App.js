42c847f37a92efa754d0698cfce67897c25b9950
Gina <hello@ginaperano.com>
2020-08-10 00:13:12 -0700

    created all basic components and added particles

--- src/App.js
+++ src/App.js
@@ -1,20 +1,39 @@
-import React from 'react';
-import Logo from './components/logo/logo';
+
+import React, { Component } from 'react';
 import Navigation from './components/navigation/navigation';
+import Particles from 'react-particles-js';
+import Logo from './components/logo/logo';
+import Rank from './components/rank/rank';
 import ImageLinkForm from './components/imagelinkform/imagelinkform';
-
-
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Navigation />
-      <Logo />
-      <ImageLinkForm />
-      {/*<FaceRecognition />*/}
-    </div>
-  );
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 30,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  }
+}
+class App extends Component {
+  render() {
+    return (
+      <div className="App" >
+        <Particles className='particles'
+          params={particlesOptions}
+        />
+        < Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm />
+        {/*<FaceRecognition />*/}
+      </div>
+    );
+  }
 }
 
 export default App;
